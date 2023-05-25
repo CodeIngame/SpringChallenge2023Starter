23defae09290ec4c4dd2dca215819afab3222685
Thomas GICQUEL <tgicquel@c2s.fr>
2023-05-25 13:17:11 +0200

    ajout du console helpers

---
+++ App.Tests/Common/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace App.Tests.Common.Helpers
+{
+    /// <summary>
+    /// Permet de simuler des entrées dans la console
+    /// </summary>
+    public static class ConsoleHelpers
+    {
+        public static void PushInReader(string row)
+        {
+            Console.SetIn(new StringReader($"{row}\r\n"));
+        }
+
+        public static void PushInReader(IEnumerable<string> row)
+        {
+            var sb = new StringBuilder();
+            row.ToList().ForEach(r => sb.Append($"{r}\r\n"));
+
+            Console.SetIn(new StringReader(sb.ToString()));
+        }
+    }
+}
